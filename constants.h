0bcc2a282eff8f1aea355d73237cef80dcb3397b
Jesse <xinthral@yahoo.com>
2020-12-30 20:08:01 -0500

    Common constants I may want to pre-define for ease of reference.

---
+++ constants.h
@@ -0,0 +1,17 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+/* This file is primarily used as global constants that
+I frequently use */
+
+namespace constants {
+    // Loop Iterator
+    constexpr int sentinal { 20 };
+}
+#endif
+
+/*
+// constants have internal linkage by default
+constexpr double pi { 3.14159 };
+constexpr double my_gravity { 9.2 }; // m/s^2 -- gravity is light on this planet
+// ... other related constants
+*/
